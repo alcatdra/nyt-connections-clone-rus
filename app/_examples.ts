dfefba68f2f4f55b69d27b48c9a7079c47cf72d8
alcatdra <alexander.menko.reggie@gmail.com>
2024-09-04 00:01:08 +0300

    added example, configured githubpages

--- app/_examples.ts
+++ app/_examples.ts
@@ -1,24 +1,24 @@
-import { Category } from "./_types";
+import { Category } from './_types';
 
 export const categories: Category[] = [
   {
-    category: 'ENCOURAGE, WITH "ON"',
-    items: ["EGG", "GOAD", "SPUR", "URGE"],
-    level: 1,
+    category: '"БЕЛЫЕ" ВЕЩИ',
+    items: ['ВОРОНА', 'ШОКОЛАД', 'НОЧЬ', 'ТАНЕЦ'],
+    level: 4,
   },
   {
-    category: "SPHERICAL FOODS",
-    items: ["JAWBREAKER", "MEATBALL", "MOZZARELLA", "ORANGE"],
-    level: 2,
-  },
-  {
-    category: "GROCERY STORE AISLES",
-    items: ["DAIRY", "FROZEN", "PRODUCE", "SNACK"],
+    category: 'ГЕРОИ БАСЕН',
+    items: ['ЛЕБЕДЬ', 'СТРЕКОЗА', 'ЛИСИЦА', 'ЩУКА'],
     level: 3,
   },
   {
-    category: "GO ___",
-    items: ["BANANAS", "FIGURE", "FISH", "STEADY"],
-    level: 4,
+    category: 'ЗНАКИ ЗОДИАКА',
+    items: ['РАК', 'СКОРПИОН', 'ЛЕВ', 'ТЕЛЕЦ'],
+    level: 2,
+  },
+  {
+    category: 'ИЗМЕРИТЕЛЬНЫЕ ПРИБОРЫ',
+    items: ['ВЕСЫ', 'ГРАДУСНИК', 'ЧАСЫ', 'ЛИНЕЙКА'],
+    level: 1,
   },
 ];
